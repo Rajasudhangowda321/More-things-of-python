3521133c16e6444adb9cff1acf29875b959d003a
Rajasudhangowda321 <69045108+Rajasudhangowda321@users.noreply.github.com>
2020-12-28 19:08:40 +0530

    Update stack two arrays vertically.py

--- Numpy 101 practice questions/stack two arrays vertically.py
+++ Numpy 101 practice questions/stack two arrays vertically.py
@@ -2,4 +2,8 @@ import numpy as np
 a = np.arange(10).reshape(2,-1)
 b = np.repeat(1, 10).reshape(2,-1)
 c=np.concatenate([a, b], axis=0)
+d=np.vstack([a,b])
+e=np.r_[a,b]
 print(c)
+print(d)
+print(e)
